6770f4199b2c48aaa71d0fd144352fd58e123217
DongSikYun <yds0903@gmail.com>
2022-08-27 09:37:33 +0900

    first

--- dockerTest/src/main/java/com/docker/test/DockerTestApplication.java
+++ dockerTest/src/main/java/com/docker/test/DockerTestApplication.java
@@ -2,10 +2,16 @@ package com.docker.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DockerTestApplication {
+public class DockerTestApplication extends SpringBootServletInitializer {
 
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(DockerTestApplication.class);
+	    }
 	public static void main(String[] args) {
 		SpringApplication.run(DockerTestApplication.class, args);
 	}
